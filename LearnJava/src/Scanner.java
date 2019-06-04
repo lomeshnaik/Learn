f5f5a4b0295287145fb28e5865b5a753c0e75444
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-03 23:58:34 -0400

    learn java Start

---
+++ LearnJava/src/Scanner.java
@@ -0,0 +1,12 @@
+public class Scanner {
+
+    static String helloMessage;
+
+    public static void main(String[] args) {
+
+        helloMessage = "Hello,  World!!!!!" ;
+        System.out.println(helloMessage);
+
+
+    }
+}
