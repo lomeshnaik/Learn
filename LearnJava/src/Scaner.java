39506942a0f3295aac4ecac98610f24f5813828f
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-04 23:26:44 -0400

    learn java Start

---
+++ LearnJava/src/Scaner.java
@@ -0,0 +1,35 @@
+
+import java.lang.System;
+import java.util.Scanner;
+public class Scaner {
+
+    static String helloMessage;
+
+    public static void main(String[] args) {
+
+        helloMessage = "Hello,  World!!!!!" ;
+        System.out.println(helloMessage);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Radius of the Circle : \n");
+        float red = sc.nextFloat();
+        System.out.println("Radius =" + red);
+
+        {
+            double pri;
+            pri = 2 * Math.PI * red ;
+            System.out.println("Perimeter= " + pri);
+
+            double area;
+            area = red * red * Math.PI ;
+            System.out.println("Area =" + area);
+
+        }
+
+
+
+
+
+    }
+}
